cc419bf3cb5e141a20ab401e9f2a06ee4376f100
Ted Wise <cwise@hdlabinc.com>
2013-02-14 10:56:47 -0500

    Add error check

--- recent-documents/sql.php
+++ recent-documents/sql.php
@@ -3,7 +3,8 @@ $database = $argv[1];
 $sql = new SQLite3( $database );
 $results = $sql->query("select path from recentdocs order by ts desc");
 
-while( $result = $results->fetchArray( SQLITE3_ASSOC ) ):
-	echo $result["path"] . "\n";
-endwhile;
-
+if (!is_null($results)) {
+	while( $result = $results->fetchArray( SQLITE3_ASSOC ) ):
+		echo $result["path"] . "\n";
+	endwhile;
+}

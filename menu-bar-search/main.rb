26112324c0b534fc37ef1c703804f66151b0b9bd
zoncoen <zoncoen@gmail.com>
2014-01-07 15:57:31 +0900

    Enable to accept multi-byte search query.

--- menu-bar-search/main.rb
+++ menu-bar-search/main.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 load 'menu_items.rb'
 
-search_term = ARGV[0].nil? ? "" : ARGV[0].downcase
+search_term = ARGV[0].nil? ? "" : ARGV[0].downcase.encode('utf-8', 'utf-8-mac')
 
 if (search_term.length > 1)
 	puts MenuItems.generate_xml(search_term, MenuItems.generate_items())

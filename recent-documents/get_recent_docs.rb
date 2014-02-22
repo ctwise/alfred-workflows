d4e5ab2b8a21aaaeb7cccff492d6b2f65b3f5baf
Ted Wise <cwise@hdlabinc.com>
2014-02-22 08:38:30 -0500

    Major update to recent documents workflow

--- recent-documents/get_recent_docs.rb
+++ recent-documents/get_recent_docs.rb
@@ -1,29 +1,31 @@
+# encoding: UTF-8
 require 'rubygems'
 load "alfred_feedback.rb"
 
-query = ARGV[0].strip.downcase
+query = (ARGV[0] || '').strip.downcase
 
-output = `osascript get_recent_documents.applescript`
+output = `osascript get_recent_documents.scpt`
 
-merge = {}
+rows = []
 
 output.split("\n").each_slice(2) do |lines|
-	merge[lines[1]] = lines[0]
+	rows << lines[1]
 end
 
 db_results = `php sql.php "#{File.expand_path('~/Library/Application Support/Alfred 2/Databases/knowledge.alfdb')}"`
 
-rows = db_results.strip.split(/\n/)
+rows << db_results.force_encoding("utf-8").strip.split(/\n/)
 
-rows.each do |row|
-	merge[row] = File.basename(row)
-end
+# Ignore files in the trash and preference panes
+rows = rows.flatten.uniq.find_all {|value| !value.include?('/.Trash/') && !value.end_with?('.prefPane')}
 
 feedback = Feedback.new
 
-merge.each do |entry|
-	if query.length == 0 || entry[1].downcase.include?(query)
-		feedback.add_item({:title => entry[1], :subtitle => entry[0], :arg => entry[0], :icon => {:type => "fileicon", :name => entry[0]}})
+rows.each do |entry|
+	basename = File.basename(entry)
+	exists = File.exists?(entry)
+	if exists && (query.length == 0 || basename.downcase.include?(query))
+		feedback.add_item({:title => basename, :subtitle => entry, :arg => entry, :icon => {:type => "fileicon", :name => entry}})
 	end
 end
 

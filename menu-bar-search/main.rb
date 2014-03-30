a33fe911f154dcfdbf801be2c1dd63da6a964f5e
Ted Wise <cwise@hdlabinc.com>
2014-03-30 16:44:15 -0400

    Stub out encode call for ruby 1.8.

--- menu-bar-search/main.rb
+++ menu-bar-search/main.rb
@@ -1,6 +1,14 @@
 # encoding: UTF-8
 load 'menu_items.rb'
 
+if RUBY_VERSION.start_with?('1.8')
+	class String
+		def encode(dst_encoding, src_encoding)
+			self
+		end
+	end
+end
+
 search_term = ARGV[0].nil? ? "" : ARGV[0].downcase.encode('utf-8', 'utf-8-mac')
 
 if (search_term.length > 1)

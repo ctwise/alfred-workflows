95a3e8b39de31ee8839b7b19f0883c02c7d753ea
Ted Wise <cwise@hdlabinc.com>
2014-08-22 10:22:26 -0400

    Fix issue with blank desktop names.

--- remote-desktop/list_desktops.rb
+++ remote-desktop/list_desktops.rb
@@ -4,8 +4,27 @@ if ARGV.length > 0
 	query = ARGV[0].downcase
 end
 home = `echo $HOME`.chomp
-raw_desktops = `plutil -p "#{home}/Library/Containers/com.microsoft.rdc.mac/Data/Library/Preferences/com.microsoft.rdc.mac.plist" | grep "}\.label\"`.lines
-desktops = raw_desktops.collect {|line| line.split("\"")[3]}.sort_by {|value| value.downcase }
+raw_bookmarks = `plutil -p "#{home}/Library/Containers/com.microsoft.rdc.mac/Data/Library/Preferences/com.microsoft.rdc.mac.plist" | grep "bookmark"`.lines
+bookmarks = {}
+raw_bookmarks.each do |line| 
+	tokens = line.split("\"")
+	bookmark_id = tokens[1].split(/[{}]/)[1]
+	bookmark_key = tokens[1].split(/\./)[3]
+	bookmark_value = tokens[3]
+	bookmarks[bookmark_id] = (bookmarks[bookmark_id] || {})
+	if bookmark_key != nil
+		bookmarks[bookmark_id][bookmark_key.to_sym] = bookmark_value
+	end
+
+	title = bookmarks[bookmark_id][:title]
+	if bookmark_key == 'label' && bookmark_value.length > 0
+		bookmarks[bookmark_id][:title] = bookmark_value
+	end
+	if bookmark_key == 'hostname' && title == nil
+		bookmarks[bookmark_id][:title] = bookmark_value
+	end
+end
+desktops = bookmarks.reject {|entry| entry.nil? }.collect { |key, value| value[:title] }
 if query.length > 0
 	desktops = desktops.find_all {|desktop| desktop.downcase.include?(query)}
 end
